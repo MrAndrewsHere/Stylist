9a35827755c389a132284b4fab1e7a8685edb93a
Ryabushkin Mikhail <ryabushkin.mikhail@gmail.com>
2018-04-03 00:05:43 +0300

    закомментил всплывающую плашку

--- src/js/dialog.js
+++ src/js/dialog.js
@@ -70,19 +70,19 @@
     }
   });
 
-  var formSettings = document.querySelector(".form-settings");
+  // var formSettings = document.querySelector(".form-settings");
 
-  formSettings.addEventListener("submit", function () {
-    event.preventDefault();
-    var node = document.createElement("div");
-    node.classList.add("success");
+  // formSettings.addEventListener("submit", function () {
+  //   event.preventDefault();
+  //   var node = document.createElement("div");
+  //   node.classList.add("success");
 
-    node.textContent = "Изменения успешно сохранены";
-    formSettings.appendChild(node);
+  //   node.textContent = "Изменения успешно сохранены";
+  //   formSettings.appendChild(node);
 
-    setTimeout(function () {
-      $(node).fadeOut("fast");
-    }, 2000);
-  });
+  //   setTimeout(function () {
+  //     $(node).fadeOut("fast");
+  //   }, 2000);
+  // });
 
 })();
