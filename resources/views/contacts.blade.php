dcd99ffa3d8946e16325d2512cbe27ac4e652f34
Ryabushkin Mikhail <ryabushkin.mikhail@gmail.com>
2018-04-02 21:59:14 +0300

    поправил номер в contacts

--- resources/views/contacts.blade.php
+++ resources/views/contacts.blade.php
@@ -29,10 +29,14 @@
         <div class="container-communication">
           <div class="communication__accent contacts__communication__accent"><b>Мы на связи с вами</b></div>
           <div>Звоните с 10:00 до 21:00 по московскому времени</div>
-          <div class="communication__contacts__phone-block"><a
-              class="phone-number communication__contacts__phone-number" href="tel:+7(843)2922222">+7 (843)
-              292-22-22</a><a class="phone-number communication__contacts__phone-number" href="tel:+79274330934">+7 9274
-              33-09-34</a></div>
+          <div class="communication__contacts__phone-block">
+            <a class="phone-number communication__contacts__phone-number" href="tel:+7(843)2922222">
+              +7 (843) 292-22-22
+            </a>
+            <a class="phone-number communication__contacts__phone-number" href="tel:+79274330934">
+              +7 (927) 433-09-34
+            </a>
+          </div>
           <div>Пишите на почту</div>
           <a class="communication__contacts__email" href="mailto:info@stilisty.com">info@stilisty.com</a>
         </div>
