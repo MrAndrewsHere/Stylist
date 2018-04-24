d1c1c50b1507c4d7fb1bffc12fb6825fda0008c0
Ryabushkin Mikhail <ryabushkin.mikhail@gmail.com>
2018-04-24 14:54:22 +0300

    сделал секцию стилисты помогут

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,24 +11,52 @@
       </p>
       <button class="btn btn--banner btn--registration">Начать преображение</button>
     </div>
-    
   </div>
-
-  <!-- <div class="slider section__home">
-    <div class="slide">
-      <div class="photo__first"><img src="img/home_client_before.jpg" width="320px" alt=""/></div>
-      <div class="photo__second"><img src="img/home_client_after.jpg" width="320px" alt=""/></div>
-    </div>
-    <div class="slide">
-      <div class="photo__first"><img src="img/client2.jpg" width="320px" alt=""/></div>
-      <div class="photo__second"><img src="img/client3.jpg" width="320px" alt=""/></div>
-    </div>
-  </div> -->
   
   <div class="container-home">
-    <div class="information">
+    <section class="information information--help">
+      <h2 class="information__title-first">
+        Cтилисты помогут
+      </h2>
+      <p class="information__title-second">
+        Выберите услугу, которая вас интересует
+      </p>
+      <div class="infotmation__categories">
+        <div class="category">
+          <img src="img/category/online-services.jpg" alt="онлайн-услуги" class="category__image">
+          <a class="category__link" href="">Онлайн услуги</a>
+          <p class="category__description">
+            Узнайте все о «своём» гардеробе, типе внешности, цветах, фасонах, получите оптимальные рекомендации онлайн в любом удобном для вас месте. 
+          </p>
+        </div>
+        <div class="category">
+          <img src="img/category/wedding.jpeg" alt="онлайн-услуги" class="category__image">
+          <a class="category__link" href="">Особый случай</a>
+          <p class="category__description">
+            Хотите получить новый стильный образ и сэкономить время на поиске идеальных вещей? Образ для вашего особого случая доверьте стилисту! Блистайте, наслаждайтесь событием или отпуском!
+          </p>
+        </div>
+        <div class="category">
+          <img src="img/category/shopping.jpg" alt="онлайн-услуги" class="category__image">
+          <a class="category__link" href="">Шоппинг-сопровождение</a>
+          <p class="category__description">
+            Творческая помощь в выборе одежды — это огромная экономия личных средств. Эффективный шоппинг-маршрут индивидуально для вас, согласно вашему бюджету, запросу, целям, пожеланиям.
+          </p>
+        </div>
+        <div class="category">
+          <img src="img/category/style-solution.jpg" alt="онлайн-услуги" class="category__image">
+          <a class="category__link" href="">Разбор стилевого решения</a>
+          <p class="category__description">
+            Каждый человек уникален, и не существует на свете другого человека, в точности похожего на Вас. Получите свою личную концепцию стиля с учетом всех характеристик.
+          </p>
+        </div>
+      </div>
+    </section>
+  </div>
+
+    <!-- <div class="information">
       <div class="infrotamtion__what">
-        <h2 class="information__title">Что это за проект?</h2>
+        
         <p class="information__text">Не всем дано сходу подобрать свой образ. Каждая женщина мечтает
           найти свой стиль и изюменку. Наши стилисты помогут вам. Также помогаем мужчинам :)</p>
       </div>
@@ -45,8 +73,19 @@
           Наши стилисты проходят тщательный контроль и вы можете быть уверены в успехе вашего
           нового образа.</p>
       </div>
+    </div> -->
+  
+
+  <!-- <div class="slider section__home">
+    <div class="slide">
+      <div class="photo__first"><img src="img/home_client_before.jpg" width="320px" alt=""/></div>
+      <div class="photo__second"><img src="img/home_client_after.jpg" width="320px" alt=""/></div>
     </div>
-  </div>
+    <div class="slide">
+      <div class="photo__first"><img src="img/client2.jpg" width="320px" alt=""/></div>
+      <div class="photo__second"><img src="img/client3.jpg" width="320px" alt=""/></div>
+    </div>
+  </div> -->
 @endsection
 
 
