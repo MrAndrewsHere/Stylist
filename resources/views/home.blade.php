ae33f6bd7ebcbaab12814ef4735e3aee7b18cca4
Ryabushkin Mikhail <ryabushkin.mikhail@gmail.com>
2018-04-24 15:19:44 +0300

    добавил символьную ссылку на автарки в свое окружение, добавил storage в gitignore

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -54,28 +54,6 @@
     </section>
   </div>
 
-    <!-- <div class="information">
-      <div class="infrotamtion__what">
-        
-        <p class="information__text">Не всем дано сходу подобрать свой образ. Каждая женщина мечтает
-          найти свой стиль и изюменку. Наши стилисты помогут вам. Также помогаем мужчинам :)</p>
-      </div>
-
-      <div class="infrotamtion__help">
-        <h2 class="information__title">Стилисты помогут</h2>
-        <p class="information__text">Мы поможем подобрать вам идеальный стиль. Наши стилисты помогут вам
-          в этом. В нашей базе более n стилистов и каждый день их количество растет.</p>
-      </div>
-
-      <div class="infrotamtion__guarantee">
-        <h2 class="information__title">Гарантия 100%</h2>
-        <p class="information__text">Вы получаете гарантию на то, что вы преобразитесь в лучшую сторону.
-          Наши стилисты проходят тщательный контроль и вы можете быть уверены в успехе вашего
-          нового образа.</p>
-      </div>
-    </div> -->
-  
-
   <!-- <div class="slider section__home">
     <div class="slide">
       <div class="photo__first"><img src="img/home_client_before.jpg" width="320px" alt=""/></div>
