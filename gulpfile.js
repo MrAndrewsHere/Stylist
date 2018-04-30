4472fa4fabcd5c76f31ddd1a54d8eb2026a4aa3f
Ryabushkin Mikhail <ryabushkin.mikhail@gmail.com>
2018-04-30 14:43:08 +0300

    отредактировал gulpfile, изменил фильтр услуг

--- gulpfile.js
+++ gulpfile.js
@@ -1,112 +1,67 @@
-// const elixir = require('laravel-elixir');
-//
-// require('laravel-elixir-vue-2');
-
-/*
- |--------------------------------------------------------------------------
- | Elixir Asset Management
- |--------------------------------------------------------------------------
- |
- | Elixir provides a clean, fluent API for defining some basic Gulp tasks
- | for your Laravel application. By default, we are compiling the Sass
- | file for your application as well as publishing vendor resources.
- |
- */
-
-// elixir((mix){
-//     mix.sass('app.scss')
-//        .webpack('app.js');
-//     });
-// elixir(function(mix) {
-//     mix.browserSync({
-//         proxy: 'auth.app',
-//         port: 30011 // здесь укажем свободный порт
-//     });
-//     mix.sass('app.scss').webpack('app.js');
-// });
-
-
-const gulp = require("gulp"),
-  blade = require("gulp-blade"),
-  sass = require("gulp-sass"),
-  plumber = require("gulp-plumber"), // отлавливает ошибки в себя
-  browserSync = require("browser-sync").create(), // перезагрузка страницы после изменения файлов
-  imagemin = require("gulp-imagemin"), // оптимизация изображений
-  newer = require("gulp-newer"), // добавляет только новые файлы
-  rename = require("gulp-rename"), // переименовывает файлы
-  svgstore = require("gulp-svgstore"), // создает svg спрайт
-  svgmin = require("gulp-svgmin"), // минифицирует svg
+const gulp = require('gulp'),
+  blade = require('gulp-blade'),
+  sass = require('gulp-sass'),
+  plumber = require('gulp-plumber'), // отлавливает ошибки в себя
+  browserSync = require('browser-sync').create(), // перезагрузка страницы после изменения файлов
+  imagemin = require('gulp-imagemin'), // оптимизация изображений
+  newer = require('gulp-newer'), // добавляет только новые файлы
+  rename = require('gulp-rename'), // переименовывает файлы
+  svgstore = require('gulp-svgstore'), // создает svg спрайт
+  svgmin = require('gulp-svgmin'), // минифицирует svg
   // del = require("del"), // удаляет файлы и папки
-  run = require("run-sequence"), // для последовательного запуска задач
-  autoprefixer = require("gulp-autoprefixer"); // добавляет автопрефиксеры
+  run = require('run-sequence'), // для последовательного запуска задач
+  autoprefixer = require('gulp-autoprefixer'); // добавляет автопрефиксеры
 
 
 const path = {
-  root: "/",
+  root: '/',
   src: {
-    pug: "src/pug/**/*.pug",
-    scss: "src/scss/style.scss",
-    js: "src/js/*.js",
-    img: "src/img/**/*.*",
-    spritesvg: "src/img/icons/*.svg",
-    fonts: "src/fonts/**/*.{woff,woff2}"
+    pug: 'src/pug/**/*.pug',
+    scss: 'src/scss/style.scss',
+    js: 'src/js/*.js',
+    img: 'src/img/**/*.*',
+    spritesvg: 'src/img/icons/*.svg',
+    fonts: 'src/fonts/**/*.{woff,woff2}'
   },
   build: {
-    pug: "public/",
-    scss: "public/css/",
-    js: "public/js/",
-    img: "public/img/",
-    fonts: "public/fonts/"
+    pug: 'public/',
+    scss: 'public/css/',
+    js: 'public/js/',
+    img: 'public/img/',
+    fonts: 'public/fonts/'
   },
   watch: {
-    pug: "src/pug/**/*.pug",
-    scss: "src/scss/**/*.scss",
-    js: "src/js/*.js",
-    img: ["!src/img/icons/*", "src/img/**/*.*"],
-    spritesvg: "src/img/icons/*.svg",
-    fonts: "src/fonts/*.{woff,woff2}"
+    pug: 'src/pug/**/*.pug',
+    scss: 'src/scss/**/*.scss',
+    js: 'src/js/*.js',
+    img: ['!src/img/icons/*', 'src/img/**/*.*'],
+    spritesvg: 'src/img/icons/*.svg',
+    fonts: 'src/fonts/*.{woff,woff2}'
   },
-  // clean: {
-  //   blocks: "public/blocks/*.*",
-  //   css: "public/css/*.*",
-  //   fonts: "public/fonts/*.*",
-  //   img: "public/img/*.*",
-  //   js: "public/js/*.*"
-  // }
 };
 
-// html
-// gulp.task("html", function () {
-//     return gulp
-//         .src(path.src.pug)
-//         .pipe(pug({ pretty: true }))
-//         // .pipe(blade())
-//         .pipe(rename({extname:'.blade.php'}))
-//         .pipe(gulp.dest('resources/views'))
-//         .pipe(browserSync.stream());
-// });
-gulp.task("compile", function () {
-  gulp.src("*.blade")
+gulp.task('compile', function () {
+  gulp.src('*.blade')
     .pipe(blade())
-    .pipe(gulp.dest("resources/views"));
+    .pipe(gulp.dest('resources/views'));
 });
 
 // css
-gulp.task("css", function () {
+gulp.task('css', function () {
   return gulp
     .src(path.src.scss)
     .pipe(plumber())
-    .pipe(sass.sync().on("error", sass.logError))
+    .pipe(sass.sync().on('error', sass.logError))
     .pipe(sass())
     .pipe(autoprefixer({
-      browsers: ["last 2 versions"]
+      browsers: ['last 2 versions']
     }))
     .pipe(gulp.dest(path.build.scss))
     .pipe(browserSync.stream());
 });
 
 //js
-gulp.task("js", function () {
+gulp.task('js', function () {
   return gulp
     .src(path.src.js)
     .pipe(gulp.dest(path.build.js))
@@ -114,7 +69,7 @@ gulp.task("js", function () {
 });
 
 // img
-gulp.task("img", function () {
+gulp.task('img', function () {
   return gulp
     .src(path.src.img)
     .pipe(newer(path.build.img))
@@ -122,9 +77,9 @@ gulp.task("img", function () {
     .pipe(browserSync.stream());
 });
 
-gulp.task("imgmin", ["img"], function () {
+gulp.task('imgmin', ['img'], function () {
   return gulp
-    .src("public/img/**/*{jpg,png}")
+    .src('public/img/**/*{jpg,png}')
     .pipe(
       imagemin([
         imagemin.optipng({ optimizationLevel: 3 }),
@@ -136,18 +91,18 @@ gulp.task("imgmin", ["img"], function () {
 });
 
 // svg-sprite
-gulp.task("spritesvg", function () {
+gulp.task('spritesvg', function () {
   return gulp
     .src(path.src.spritesvg)
     .pipe(svgmin())
     .pipe(svgstore({ inlineSvg: true }))
-    .pipe(rename("spritesvg.svg"))
+    .pipe(rename('spritesvg.svg'))
     .pipe(gulp.dest(path.build.img))
     .pipe(browserSync.stream());
 });
 
 // fonts
-gulp.task("fonts", function () {
+gulp.task('fonts', function () {
   return gulp
     .src(path.src.fonts)
     .pipe(newer(path.build.fonts))
@@ -155,36 +110,27 @@ gulp.task("fonts", function () {
     .pipe(browserSync.stream());
 });
 
-// clean
-// gulp.task("clean", function () {
-//   return del(path.clean.blocks, { read: false }),
-//   del(path.clean.css, { read: false }),
-//   del(path.clean.fonts, { read: false }),
-//   del(path.clean.img, { read: false }),
-//   del(path.clean.js, { read: false });
-// });
-
 // public
-gulp.task("public", function (fn) {
-  run( "css", "js", "imgmin", "spritesvg", "fonts", fn);
+gulp.task('public', function (fn) {
+  run('css', 'js', 'imgmin', 'spritesvg', 'fonts', fn);
 });
 
 // server
-gulp.task("serve", function () {
-  //browserSync.init({
-  //  server: "public",
-  // notify: false
-  //});
+gulp.task('serve', function () {
+  browserSync.init({
+    proxy: '127.0.0.1',
+    port: 8080
+  });
 
-  gulp.watch(path.watch.scss, ["css"]);
-  gulp.watch(path.watch.js, ["js"]);
-  gulp.watch(path.watch.img, ["imgmin"]);
-  gulp.watch(path.watch.spritesvg, ["spritesvg"]);
-  gulp.watch(path.watch.fonts, ["fonts"]);
+  gulp.watch(path.watch.scss, ['css']);
+  gulp.watch(path.watch.js, ['js']);
+  gulp.watch(path.watch.img, ['imgmin']);
+  gulp.watch(path.watch.spritesvg, ['spritesvg']);
+  gulp.watch(path.watch.fonts, ['fonts']);
 });
 
 // default
-gulp.task("default", function (fn) {
-  run("public", ["serve"], fn);
+gulp.task('default', function (fn) {
+  run('public', ['serve'], fn);
 });
 
--- resources/views/services.blade.php
+++ resources/views/services.blade.php
@@ -13,7 +13,7 @@
         <a href="/services/Особый случай" class="btn btn--filter">Особый случай</a>
         <a href="/services/Онлайн услуги" class="btn btn--filter">Онлайн услуги</a>
       </div>
-      <span>@if(Session::has('success')) {{Session::get('success')}} @endif</span>
+
       @if(isset($Categoryservices))
       @foreach($Categoryservices as $service)
         <div class="card card__margin">
@@ -26,26 +26,9 @@
             <div class="card__description__text"><b>Результат </b><span
                 class="card__description__result">{{$service->result}}</span></div>
             <div class="card__choose-price">
-              <div class="card__choose-price-block">
-                <div class="card__choose-price__category"><b>Vip стилист</b></div>
-                <div class="card__choose-price__cost">
-                    <button type="submit"  class="btn btn--action btn--action-small">
-                      3500 р/час
-                    </button>
-                </div>
-              </div>
-              <div class="card__choose-price-block">
-                <div class="card__choose-price__category"><b>Стилист первой категории</b></div>
-                <div class="card__choose-price__cost">
-                  <button class="btn btn--action btn--action-small">2500 р/час</button>
-                </div>
-              </div>
-              <div class="card__choose-price-block">
-                <div class="card__choose-price__category"><b>Начинающий стилист</b></div>
-                <div class="card__choose-price__cost">
-                  <button class="btn btn--action btn--action-small">1500 р/час</button>
-                </div>
-              </div>
+              <button type="submit" class="btn btn--action btn--action-small">
+                Подробнее
+              </button>
             </div>
           </div>
         </div>
@@ -54,63 +37,3 @@
     </div>
   </section>
 @endsection
-
-{{--<script>--}}
-    {{--function OnLoad(e) {--}}
-        {{--var xhr = new XMLHttpRequest(); //Создаём новый объект XMLHttpRequest--}}
-
-        {{--xhr.open('POST', '/take', true); // Конфигурируем его запрос--}}
-
-        {{--var id= encodeURIComponent(e.value);--}}
-        {{--xhr.setRequestHeader('Content-Type','application/x-www-form-urlencoded');--}}
-        {{--xhr.send('id='+id); // Отсылаем запрос--}}
-
-        {{--xhr.onreadystatechange = function () {--}}
-            {{--if (xhr.readyState != 4) return; // 4 - запрос завершен--}}
-            {{--if (xhr.status != 200) {--}}
-{{--// обработать ошибку--}}
-                {{--alert(xhr.status + ': ' + xhr.statusText);--}}
-            {{--} else {--}}
-                {{--try {--}}
-                    {{--var phones = JSON.parse(xhr.responseText);--}}
-                {{--} catch (e) {--}}
-                    {{--alert("Некорректный ответ " + e.message);--}}
-                {{--}--}}
-
-{{--// что должно произойти--}}
-
-                {{--showPhones(phones);--}}
-
-                {{--button.innerHTML = 'Загружено';--}}
-                {{--button.disabled = false;--}}
-            {{--}--}}
-        {{--}--}}
-
-        {{--button.innerHTML = 'Загрузка...';--}}
-        {{--button.disabled = true;--}}
-    {{--}--}}
-
-    {{--function showPhones(phones) {--}}
-        {{--while (list.firstChild) {--}}
-            {{--list.removeChild(list.firstChild);--}}
-        {{--}--}}
-
-        {{--phones.forEach(function (phone) {--}}
-            {{--var img = list.appendChild(document.createElement('img'));--}}
-            {{--img.src = phone.photo;--}}
-
-            {{--var name = list.appendChild(document.createElement('p'));--}}
-            {{--name.innerHTML = phone.name;--}}
-        {{--});--}}
-    {{--}--}}
-
-        {{--var category = shop.value();--}}
-        {{--alert(category);--}}
-        {{--category = encodeURIComponent(category);--}}
-        {{--var xhr = new XMLHttpRequest();--}}
-        {{--xhr.open('GET','/take'+'category='+category,true);--}}
-        {{--// xhr.setRequestHeader('Content-Type','application/x-www-form-urlencoded');--}}
-        {{--xhr.send();--}}
-
-
-{{--</script>--}}
