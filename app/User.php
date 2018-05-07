1f87e59bd4d97da49eba0d6c2da4969be23581b4
MrAndrewsHere <Andrews.mr@yandex.ru>
2018-05-08 00:27:35 +0300

    фамилия стилиста

--- app/User.php
+++ app/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable
    * @var array
    */
   protected $fillable = [
-    'name', 'email', 'password', 'role_id', 'avatar', 'city'
+    'name', 'second_name','email', 'password', 'role_id', 'avatar', 'city'
   ];
 
   /**
