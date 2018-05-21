144fa25083146057e1b81c498a117db3a842247d
MrAndrewsHere <32438685+MrAndrewsHere@users.noreply.github.com>
2018-05-21 15:10:46 +0300

    fix

--- resources/views/settings.blade.php
+++ resources/views/settings.blade.php
@@ -80,8 +80,10 @@
           <label>Аватар
             <input type="file" id="avatar" name="avatar" class="form__input"/>
           </label>
-
-          <span id="form__output-avatar"></span>
+         
+          <span id="form__output-avatar">
+             <img class="form__output-avatar" src="{{Auth::user()->avatar}}" alt="{{Auth::user()->name.' '.Auth::user()->second_name}}">
+          </span>
 
           <label>Имя
             <input class="form__input" name="name" type="text" value="{{$currentUser->name}}" required="required"/>
