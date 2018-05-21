7136ba88a58b95b920fe8cb0ced13a8bda2abcfb
MrAndrewsHere <32438685+MrAndrewsHere@users.noreply.github.com>
2018-05-21 14:55:39 +0300

    jnkjn

--- resources/views/settings.blade.php
+++ resources/views/settings.blade.php
@@ -23,7 +23,7 @@
           
           <!-- написать проверку, если в бд у пользователя есть аватарка, то вывести её в <img class="form__output-avatar" src="ссылка на фотку" alt="ссылка на имя + фамилия пользователя">-->
           <span id="form__output-avatar">
-            <!-- ................. -->
+            <img class="form__output-avatar" src="{{Auth::user()->avatar}}" alt="{{Auth::user()->name.' '.Auth::user()->second_name}}">
           </span>
 
           <label>Имя
