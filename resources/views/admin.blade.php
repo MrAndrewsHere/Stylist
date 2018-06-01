fe805652fef1dd17b73fb6618b5037abbb639317
Ryabushkin Mikhail <ryabushkin.mikhail@gmail.com>
2018-06-02 00:55:57 +0300

    add -- иконка уведомлений (.notification)

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -6,7 +6,7 @@
   <section class="section section--admin">
     <h1 class="section__title">Панель администратора</h1>
     <div class="container">
-      <h2 class="title-second">Новые заявки <span class="alert">({{$stylists->count()}})</span></h2>
+      <h2 class="title-second">Новые заявки <span class="notification">{{$stylists->count()}}</span></h2>
       <div class="admin-request">
         <div class="admin-request__new">
           <h3 class="title-block">Стилисты:</h3>
