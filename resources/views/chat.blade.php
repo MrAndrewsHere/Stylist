c35875e6612a8e584630233b95544f1bccab3aba
MrAndrewsHere <Andrews.mr@yandex.ru>
2018-11-09 08:57:51 +0300

    Update resources/views/chat.blade.php

--- resources/views/chat.blade.php
+++ resources/views/chat.blade.php
@@ -4,31 +4,31 @@
 @section('content')
 <div class="container">
 
-    {{--<div id="app">--}}
-        {{--<div class="container-communication">--}}
-            {{--<div class="panel-heading">Chats</div>--}}
-            {{--<button class="btn btn-primary btn-sm" id="btn-chat"  @click='changePeer("1")'>--}}
-                {{--1--}}
-            {{--</button>--}}
-            {{--<button class="btn btn-primary btn-sm" id="btn-chat" @click='changePeer("2")'>--}}
-                {{--2--}}
-            {{--</button>--}}
-            {{--<button class="btn btn-primary btn-sm" id="btn-chat"  @click='changePeer("3")'>--}}
-                {{--3--}}
-            {{--</button>--}}
-            {{--<div id = "panel_body_id" class="panel-body">--}}
-                {{--<chat-messages :messages="messages"  :user="{{ Auth::user() }}"></chat-messages>--}}
-            {{--</div>--}}
-            {{--<div class="panel-footer">--}}
-                {{--<chat-form--}}
-                        {{--v-on:messagesent="addMessage"--}}
-                        {{--v-on:sendinguser="currentUser"--}}
-                        {{--:user="{{ Auth::user() }}"--}}
+    <div id="app">
+        <div class="container-communication">
+            <div class="panel-heading">Chats</div>
+            <button class="btn btn-primary btn-sm" id="btn-chat"  @click='changePeer("1")'>
+                1
+            </button>
+            <button class="btn btn-primary btn-sm" id="btn-chat" @click='changePeer("2")'>
+                2
+            </button>
+            <button class="btn btn-primary btn-sm" id="btn-chat"  @click='changePeer("3")'>
+                3
+            </button>
+            <div id = "panel_body_id" class="panel-body">
+                <chat-messages :messages="messages"  :user="{{ Auth::user() }}"></chat-messages>
+            </div>
+            <div class="panel-footer">
+                <chat-form
+                        v-on:messagesent="addMessage"
+                        v-on:sendinguser="currentUser"
+                        :user="{{ Auth::user() }}"
 
 
-                {{--></chat-form>--}}
-            {{--</div>--}}
-        {{--</div>--}}
-    {{--</div>--}}
+                ></chat-form>
+            </div>
+        </div>
+    </div>
 </div>
 @endsection
